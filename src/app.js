3d82d3dce1a039a2ae898feae08d3f65d1fd8802
Sergio Luciano Pérez <slperez@proveedor.teco.com.ar>
2023-11-20 03:52:38 -0300

    se agregan metodos get products, get con query y get con params

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import ProductManager from './managers/ProductManager.js';
 
-const PORT = 8080
+const PORT = 8081
 
 const app = express();
 
@@ -11,102 +11,36 @@ const path = './src/files/products.json';
 
 const manager = new ProductManager(path)
 
-// app.listen(PORT, ()=>{
-//     console.log(`Server listening on ${PORT}`)
-// })
+app.listen(PORT, ()=>{
+    console.log(`Server listening on ${PORT}`)
+})
 
-let producto2 = {
-    title : 'producto prueba 2',
-    description : 'Esto es un producto prueba 2',
-    price : 2000,
-    thumbnail : 'Sin imagen',
-    code : 'abc122',
-    stock : 25
-}
-
-let producto3 = {
-    title : 'producto prueba 3',
-    description : 'Esto es un producto prueba 3',
-    price : 300,
-    thumbnail : 'Sin imagen',
-    code : 'abc121',
-    stock : 25
-}
-
-let producto4 = {
-    title : 'producto prueba 4',
-    description : 'Esto es un producto prueba 4',
-    price : 400,
-    thumbnail : 'Sin imagen',
-    code : 'abc124',
-    stock : 25
-}
-
-let producto5 = {
-    title : 'producto prueba 5',
-    description : 'Esto es un producto prueba 5',
-    price : 500,
-    thumbnail : 'Sin imagen',
-    code : 'abc125',
-    stock : 25
-}
-
-let producto6 = {
-    title : 'producto prueba 6',
-    description : 'Esto es un producto prueba 6',
-    price : 600,
-    thumbnail : 'Sin imagen',
-    code : 'abc126',
-    stock : 25
-}
-
-let producto7 = {
-    title : 'producto prueba 7',
-    description : 'Esto es un producto prueba 7',
-    price : 700,
-    thumbnail : 'Sin imagen',
-    code : 'abc127',
-    stock : 25
-}
-
-let producto8 = {
-    title : 'producto prueba 8',
-    description : 'Esto es un producto prueba 8',
-    price : 800,
-    thumbnail : 'Sin imagen',
-    code : 'abc128',
-    stock : 25
-}
-
-let producto9 = {
-    title : 'producto prueba 9',
-    description : 'Esto es un producto prueba 9',
-    price : 900,
-    thumbnail : 'Sin imagen',
-    code : 'abc129',
-    stock : 25
-}
-
-let producto10 = {
-    title : 'producto prueba 10',
-    description : 'Esto es un producto prueba 10',
-    price : 1000,
-    thumbnail : 'Sin imagen',
-    code : 'abc130',
-    stock : 25
-}
-
-
-
-// let productos = await manager.addProduct(producto2)
-// productos = await manager.addProduct(producto3)
-// productos = await manager.addProduct(producto4)
-// productos = await manager.addProduct(producto5)
-// productos = await manager.addProduct(producto6)
-// productos = await manager.addProduct(producto7)
-// productos = await manager.addProduct(producto8)
-// productos = await manager.addProduct(producto9)
-// productos = await manager.addProduct(producto10)
+app.get('/products', async (req, res)=>{
     
+    let products = await manager.getProducts();
+    let limit = parseInt(req.query.limit)
+
+    if(!limit || limit > products.length){
+        res.json({products})
+    } else {
+        let productsLimit = products.slice(0, limit)
+        res.json({products: productsLimit})
+    }
+})
+
+app.get('/products/:id', async (req, res)=>{
+    const products = await manager.getProducts();
+
+    const idProduct = parseInt(req.params.id)
+
+    const producto = products.find(prod => prod.id === idProduct)
+
+    if (!producto) {
+        return res.send({error: 'Not Found'}) 
+    }
+    res.json({producto: producto})
+})
+
+ 
 
 
